c9c8759b0353e5f945760819e2faa492d29c88fa
Markus Kahl <machisuji@gmail.com>
2018-03-05 10:52:05 +0000

    added database.yml including mysql options

--- ci/customize.sh
+++ ci/customize.sh
@@ -6,7 +6,8 @@ cp ${CUSTOMIZATION_DIR}/.travis.yml .
 
 # Copy custom gemfiles
 cp ${CUSTOMIZATION_DIR}/Gemfile.plugins .
+cp ${CUSTOMIZATION_DIR}/database.yml config/
 
-git add -f .travis.yml Gemfile.plugins
+git add -f .travis.yml Gemfile.plugins config/database.yml
 
 popd
